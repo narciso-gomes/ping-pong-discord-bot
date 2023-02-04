d6675e7914048bfbbbf16a0979eb95de4b3a4615
Narciso Gomes <narcisogomes@MacBook-Air-de-Narciso.local>
2023-02-04 01:05:23 -0300

    First commit

---
+++ start-bot.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const Discord = require("discord.js")
+
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+
+  if (msg.content === "PING") {
+    msg.reply("PONG");
+  }
+
+})
+
+client.login(process.env.TOKEN)
